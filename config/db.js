6e490f267e9f469a71d1fc16b20da543ff8da3c6
stephenthajeb <13518150@std.stei.itb.ac.id>
2021-02-23 00:33:27 +0700

    environment variable management

--- config/db.js
+++ config/db.js
@@ -1,19 +1,39 @@
 const mongoose = require('mongoose')
 const config = require('config')
-const db = config.get('MONGOURI')
 
+// Connect to mongo docker
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    })
+    await mongoose.connect(
+      `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`,
+      {
+        useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+      },
+    )
     console.log('Connected to DB')
   } catch (err) {
     console.error(err.message)
   }
 }
+
+// Connect to MongoCloud
+
+// const db = config.get('MONGOURI')
+// const connectDB = async () => {
+//   try {
+//     await mongoose.connect(db, {
+//       useNewUrlParser: true,
+//       useCreateIndex: true,
+//       useUnifiedTopology: true,
+//       useFindAndModify: false,
+//     })
+//     console.log('Connected to DB')
+//   } catch (err) {
+//     console.error(err.message)
+//   }
+// }
 
 module.exports = connectDB
